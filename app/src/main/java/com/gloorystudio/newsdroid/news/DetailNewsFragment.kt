304e7ff1ab5318bd2a8ce9f07479ceb6c812f03c
ibrahim-ekinci <halil.ibrahim.ekinci@gmail.com>
2021-09-10 20:37:48 +0300

    Toolbar eklendi.

--- app/src/main/java/com/gloorystudio/newsdroid/news/DetailNewsFragment.kt
+++ app/src/main/java/com/gloorystudio/newsdroid/news/DetailNewsFragment.kt
@@ -2,11 +2,11 @@ package com.gloorystudio.newsdroid.news
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ScrollView
 import com.gloorystudio.newsdroid.R
 import com.gloorystudio.newsdroid.databinding.FragmentDetailNewsBinding
 
-class DetailNewsFragment : BaseNewFragment<FragmentDetailNewsBinding>(R.layout.fragment_detail_news) {
+class DetailNewsFragment :
+    BaseNewFragment<FragmentDetailNewsBinding>(R.layout.fragment_detail_news) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.news = news
